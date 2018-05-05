e31f66efe711c53eac8f3cafcfc0dfaa7e1c7ee0
yuslianto <yuslianto56@gmail.com>
2018-05-05 12:26:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 
 import nav from './nav';
 import paymentReducer from './payment';
-import usersReducer from './users';
 
 const reducers = combineReducers({
   nav,
-  paymentReducer,
-  usersReducer,
+
 });
 
 export default reducers;
